72b8e7361fe5af7889f278cc39068f0be514f07f
johryq <36503495+johryq@users.noreply.github.com>
2020-03-17 10:57:47 +0800

    Add files via upload

---
+++ hello.cs
@@ -0,0 +1,12 @@
+using System
+
+namespace Hello
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
